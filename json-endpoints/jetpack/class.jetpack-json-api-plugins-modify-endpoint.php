e1251b311888e398c13e0750d24ada95ab8064e8
Enej Bajgoric <enej.bajgoric@gmail.com>
2017-10-16 14:23:35 -0700

    Plugin API: Update the plugin permissions so that they are more compatilble with WP 4.9
    in 4.9 developers can define plugin specific permission.
    This PR tries to address that in our APIs.

--- json-endpoints/jetpack/class.jetpack-json-api-plugins-modify-endpoint.php
+++ json-endpoints/jetpack/class.jetpack-json-api-plugins-modify-endpoint.php
@@ -84,7 +84,15 @@ class Jetpack_JSON_API_Plugins_Modify_Endpoint extends Jetpack_JSON_API_Plugins_
 	}
 
 	protected function activate() {
+		$permission_error = false;
 		foreach ( $this->plugins as $plugin ) {
+			if ( ! $this->single_plugin_current_user_can( 'activate_plugin', $plugin ) ) {
+				$this->log[ $plugin ]['error'] = __( 'Sorry, you are not allowed to activate this plugin.' );
+				$has_errors = true;
+				$permission_error = true;
+				continue;
+			}
+
 			if ( ( ! $this->network_wide && Jetpack::is_plugin_active( $plugin ) ) || is_plugin_active_for_network( $plugin ) ) {
 				$this->log[ $plugin ]['error'] = __( 'The Plugin is already active.', 'jetpack' );
 				$has_errors = true;
@@ -117,16 +125,37 @@ class Jetpack_JSON_API_Plugins_Modify_Endpoint extends Jetpack_JSON_API_Plugins_
 			}
 			$this->log[ $plugin ][] = __( 'Plugin activated.', 'jetpack' );
 		}
-		if ( ! $this->bulk && isset( $has_errors ) ) {
+
+		if ( ! $this->bulk && isset( $has_errors )  ) {
 			$plugin = $this->plugins[0];
+			if ( $permission_error ) {
+				return new WP_Error( 'unauthorized_error', $this->log[ $plugin ]['error'], 404 );
+			}
 			return new WP_Error( 'activation_error', $this->log[ $plugin ]['error'] );
 		}
 	}
 
+	protected function single_plugin_current_user_can( $capability, $plugin ) {
+		global $wp_version;
+		if ( version_compare( $wp_version, '4.9-beta2' ) >= 0 ) {
+			return current_user_can( $capability, $plugin );
+		}
+		// Assume that the user has the capability...
+		return true;
+
+	}
+
 	protected function deactivate() {
+		$permission_error = false;
 		foreach ( $this->plugins as $plugin ) {
 			if ( ! Jetpack::is_plugin_active( $plugin ) ) {
 				$error = $this->log[ $plugin ]['error'] = __( 'The Plugin is already deactivated.', 'jetpack' );
+				continue;
+			}
+
+			if ( ! $this->single_plugin_current_user_can('deactivate_plugin', $plugin ) ) {
+				$error = $this->log[ $plugin ]['error'] = __( 'Sorry, you are not allowed to deactivate this plugin.' );
+				$permission_error = true;
 				continue;
 			}
 
@@ -144,6 +173,9 @@ class Jetpack_JSON_API_Plugins_Modify_Endpoint extends Jetpack_JSON_API_Plugins_
 			$this->log[ $plugin ][] = __( 'Plugin deactivated.', 'jetpack' );
 		}
 		if ( ! $this->bulk && isset( $error ) ) {
+			if ( $permission_error ) {
+				return new WP_Error( 'unauthorized_error', $error, 403 );
+			}
 			return new WP_Error( 'deactivation_error', $error );
 		}
 	}
